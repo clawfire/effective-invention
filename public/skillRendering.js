a811427dbe510975327ac6477213f1a8d4b8c810
Thibault Milan <hello@thibaultmilan.com>
2024-10-25 16:02:29 +0200

    Feat: display priority on the list

--- public/skillRendering.js
+++ public/skillRendering.js
@@ -10,39 +10,38 @@
  * @param {number} skill.priority - The priority of the skill between 0 and 8.
  * @param {string} skill.uri - The URI of the skill.
  *
- * @param {string} source - The source of the skill (either AI or ESCO)
- *
  * @returns {HTMLElement} - The DOM element representing the skill.
  */
-function renderSkill(skill, source) {
+function renderSkill(skill) {
     const skillElement = document.createElement('li');
     skillElement.classList.add('skill');
 
-    // display the skill priority
-    const priorityElement = document.createElement('span');
-    priorityElement.classList.add('badge');
-    // add class based on priority
-    // priority 0-2: green (.success), 3-5: yellow (.warning), 6-8: red (.alert)
-    priorityElement.classList.add(`badge-${skill.priority >= 0 && skill.priority <= 2?'success' : skill.priority >= 3 && skill.priority <= 5? 'warning' : 'alert'}`);
-    // set priority
-    priorityElement.textContent = skill.priority;
-    skillElement.appendChild(priorityElement);
-
+    // display the skill priority if skill.priority is provided
+    if (skill.priority) {
+        const priorityElement = document.createElement('span');
+        priorityElement.classList.add('badge');
+        // add class based on priority
+        // priority 0-2: green (.success), 3-5: yellow (.warning), 6-8: red (.alert)
+        priorityElement.classList.add(`${skill.priority >= 0 && skill.priority <= 2?'success' : skill.priority >= 2 && skill.priority <= 5? 'warning' : 'alert'}`);
+        // set priority
+        priorityElement.textContent = Math.round(skill.priority);
+        skillElement.appendChild(priorityElement);
+    }
     // display the skill name
     const nameElement = document.createElement('span');
     nameElement.classList.add('name');
-    nameElement.textContent = skill.name;
+    nameElement.textContent = skill.name ? " " + skill.name + " " : " " + skill.matchedSkill.name + " ";
     skillElement.appendChild(nameElement);
 
     //display the skill source with a label
     const sourceElement = document.createElement('span');
     sourceElement.classList.add('label');
     // if source is AI, add label class .success, else add label class .warning
-    sourceElement.classList.add(`${source === 'AI'?'success' : 'warning'}`);
-    sourceElement.textContent = source;
+    sourceElement.classList.add(`${skill.name ? 'success' : 'info'}`);
+    sourceElement.textContent = `${skill.name? ' AI' : ' ESCO'}`;
     // add icon based on source
     // if it's AI, add a i.fa-solid.fa-sparkles class, else add a i.fa-solid.fa-brain class
-    sourceElement.innerHTML += ` <i class="fa ${source === 'AI'?'fa-solid fa-sparkles' : 'fa-solid fa-brain'}"></i>`;
+    sourceElement.innerHTML += ` <i class="fa ${skill.name ?'fa-solid fa-sparkles' : 'fa-solid fa-brain'}"></i>`;
     skillElement.appendChild(sourceElement);
 
 
